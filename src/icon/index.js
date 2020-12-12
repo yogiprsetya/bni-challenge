d632f66eeecd2e000a0559928b74b6855603b2e0
Yogi Prsetya <yogiprsetya@gmail.com>
2020-12-12 15:14:00 +0700

    UI Login

--- src/icon/index.js
+++ src/icon/index.js
@@ -10,7 +10,11 @@ const Icon = ({ size, color, icon }) => (
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
   >
-    <path fill={color} d={iconPath[icon].path} />
+    {
+      iconPath[icon].path.map(item => (
+        <path fill-rule="evenodd" fill={color} d={item} />
+      ))
+    }
   </svg>
 );
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,61 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import Icon from 'icon';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [hidePassword, setHidePassword] = useState(true);
 
   return (
-    <div className="bg-darkDrop h-full">
-      <div className="max-w-md m-auto">
+    <div className="bg-darkDrop h-screen overflow-hidden flex justify-center items-center">
+      <div className="w-3/12">
         <img src="/images/logo.jpg" className="mx-auto" alt="logo" />
 
         <form>
-          <div className="text-white px-5 mt-1 py-3">
+          <div className="text-white mt-1 py-3">
             <small className="text-mainBrand">Email</small>
 
-            <div className="w-full rounded border pt-1">
+            <div className="w-full rounded border">
               <input
                 value={ email }
                 required
                 onChange={e => setEmail(e.target.value)}
-                className="w-full border-white bg-transparent"
+                className="w-full px-2 border-white bg-transparent h-8"
               />
             </div>
           </div>
 
-          <div className="text-white px-5 mt-1 py-3">
-            <small className="text-mainBrand">Email</small>
+          <div className="text-white mt-1 py-3">
+            <small className="text-mainBrand">Password</small>
 
-            <div className="w-full rounded border pt-1">
-              <input
-                value={ email }
-                required
-                onChange={e => setEmail(e.target.value)}
-                className="w-full border-white bg-transparent"
-              />
+            <div className="w-full relative rounded border">
+              <div className="flex items-center">
+                <input
+                  value={ password }
+                  required
+                  type={ hidePassword ? 'password' : 'text' }
+                  onChange={e => setPassword(e.target.value)}
+                  className="w-full px-2 border-white bg-transparent h-8"
+                />
 
-              <button
-                classname="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label"
-              >
-                show
-              </button>
+                <button
+                  type="button"
+                  className="absolute right-2"
+                  onClick={ () => setHidePassword(!hidePassword) }
+                >
+                  <Icon icon={ hidePassword ? 'eye-off' : 'eye-on' } />
+                </button>
+              </div>
             </div>
           </div>
+
+          <button className="bg-mainBrand w-full h-9 rounded mt-5 text-sm">
+            MASUK
+          </button>
         </form>
+
+        <button className="text-mainBrand m-auto mt-8 block">Lupa Password?</button>
       </div>
     </div>
   )
